94c1aa41199a546347da367316502e4ed673f41b
Spiciu3 <u56114@student.wat.edu.pl>
2025-10-29 11:01:17 +0100

    WiP: added function to added new user

--- main.py
+++ main.py
@@ -10,16 +10,23 @@ def user_info(users_data: list) -> None:
         print(f"Twój znajomy {user["name"]} z miejscowości {user["location"]} opublikował {user["posts"]} postów ")
 
 
+def add_user(users_data) -> None:
+    name: str = input("Podaj imie nowego znajomego: ")
+    location: str = input("Podaj nazwę miejscowosci: ")
+    posts: int = int(input("Podaj liczbę postów: "))
+    users_data.append({"name": name, "location": location, "posts": posts})
+
+
 while True:
     tmp_choice: int = int(input("Wybierz opcję menu"))
     if tmp_choice == 0:
         break
     if tmp_choice == 1:
         print("wybrano funkcje wyświetlania aktywności znajomych")
-        for user in users:
-            user_info(users)
+        user_info(users)
     if tmp_choice == 2:
         print("wybrano funkcje dodawania znajomego")
+        add_user(users)
     if tmp_choice == 3:
         print("wybrano funkcje usuwania znajomych")
     if tmp_choice == 4:
