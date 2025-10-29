aeb5117450c44d2ee8e713c3ae9664e6ff0bd2a1
Spiciu3 <u56114@student.wat.edu.pl>
2025-10-29 09:10:26 +0100

    WiP: added for loop with users

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ("Hello world")
+zmienna_1:str= 'Miłosz'
+zmienna_2:str= 'Michasia'
+zmienna_3:str= 'Kasia'
+zmienna_4:str= 'Asia'
+
+
+
+uzytkownicy:list=[zmienna_1, zmienna_2, zmienna_3, zmienna_4]
+for uzytkownik in uzytkownicy:
+    print(f"Hello {uzytkownik}")
